5a75b8da60d5b38cfc7461efec1d0aa9fecb3631
purpleoctopus <denysostapuk7@gmail.com>
2024-05-21 19:14:35 +0300

    Second Commit

---
+++ src/app/core/tickets-view/tickets-view.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tickets-view',
+  standalone: true,
+  imports: [],
+  templateUrl: './tickets-view.component.html',
+  styleUrl: './tickets-view.component.css'
+})
+export class TicketsViewComponent {
+
+}
